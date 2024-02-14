99f09db7b62f2d628e24aa63b818703f02bf131e
Josh Glover <25733402+josh-glover@users.noreply.github.com>
2024-02-13 20:18:38 -0600

    Initial entity classes

---
+++ src/BargainMagic.DataAccess.Abstractions/Entities/Card.cs
@@ -0,0 +1,9 @@
+﻿namespace BargainMagic.DataAccess.Abstractions.Entities
+{
+    public class Card
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsLegal { get; set; }
+    }
+}
