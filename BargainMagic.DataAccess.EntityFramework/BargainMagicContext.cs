374f15f0050d6c72536b63ef5f6b19d81e0bc63a
Josh Glover <25733402+josh-glover@users.noreply.github.com>
2024-02-13 20:38:08 -0600

    Initial DbContext

---
+++ BargainMagic.DataAccess.EntityFramework/BargainMagicContext.cs
@@ -0,0 +1,21 @@
+﻿using BargainMagic.DataAccess.Abstractions.Entities;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BargainMagic.DataAccess.EntityFramework
+{
+    public class BargainMagicContext : DbContext
+    {
+        public string DatabasePath { get; set; }
+
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<Season> Seasons { get; set; }
+        public DbSet<SeasonCardCost> SeasonCardCosts { get; set; }
+
+        public BargainMagicContext()
+        {
+            var applicationDirectory = Directory.GetCurrentDirectory();
+            this.DatabasePath = Path.Join(applicationDirectory, "bargainmagic.db");
+        }
+    }
+}
